865c47c3ad72df7629d5d50d8d6465c17355501b
Gabriel <gabrielcalixto9@gmail.com>
2018-02-05 19:29:47 -0200

    Add some style maneuvering with js

--- js/principal.js
+++ js/principal.js
@@ -10,19 +10,22 @@ var alturaValida = true;
 var pesoValido = true;
 var pacientes = document.querySelectorAll(".paciente");
 for(var i =0; i<pacientes.length;i++){
-    var altura = pacientes[i].querySelector(".info-altura").textContent;
-    var peso = pacientes[i].querySelector(".info-peso").textContent;
-    var TDimc=pacientes[i].querySelector(".info-imc");
+    var paciente = pacientes[i]; 
+    var altura = paciente.querySelector(".info-altura").textContent;
+    var peso = paciente.querySelector(".info-peso").textContent;
+    var TDimc=paciente.querySelector(".info-imc");
 
-    if(altura>3||altura<0)
+    if(altura>=3||altura<=0)
     {
         alturaValida=false;
         TDimc.textContent="Altura Invalida";
+        paciente.classList.add("paciente-invalido");
     }
-    if(peso<0||peso>1000)
+    if(peso<=0||peso>=1000)
     {
         pesoValido = false;
         TDimc.textContent = "peso Invalido";
+        paciente.classList.add("paciente-invalido");
     }
     if(alturaValida && pesoValido)
     {
