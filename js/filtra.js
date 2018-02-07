50a13498ad7475b298eef218b9b554898bc4623e
Gabriel <gabrielcalixto9@gmail.com>
2018-02-07 20:19:20 -0200

    add a filtering system

---
+++ js/filtra.js
@@ -0,0 +1,37 @@
+var campoFiltro = document.querySelector("#filtrar-tabela");
+campoFiltro.addEventListener("input",function () {
+    var pacientes = document.querySelectorAll(".paciente");
+    
+    if(this.value.length>0)
+        {
+          for(var i =0; i<pacientes.length;i++)
+          {
+            
+            var paciente = pacientes[i];
+            var tdNome = paciente.querySelector(".info-nome");
+            var nome = tdNome.textContent;
+            var exp = new RegExp(this.value,"i");
+       
+
+            if(!exp.test(nome))
+            {
+                paciente.classList.add("invisible");
+            }
+                else
+                {
+                    paciente.classList.remove("invisible");
+                }
+        }
+    }
+            else
+            {
+                for(var i =0; i<pacientes.length;i++)
+                {
+                    var paciente = pacientes[i];
+                    paciente.classList.remove("invisible");
+                }
+
+            }
+    
+    
+});
